65ba8e2713a0a9a8b34d81bb238e8ce7496c1f0f
NouranAbdelaziz <nouran.abdelaziz@aucegpt.edu>
2024-05-01 13:56:45 +0300

    added wishbone wrapper verification

--- verify/uvm-python/top.v
+++ verify/uvm-python/top.v
@@ -17,19 +17,19 @@ module top();
         wire [31:0]	PRDATA;
         wire 		PREADY;
         EF_I2S_APB uut(
-        .ws(ws),
-        .sck(sck),
-        .sdi(sdi),
-        .PCLK(CLK),
-        .PRESETn(RESETn),
-        .PADDR(PADDR),
-        .PWRITE(PWRITE),
-        .PSEL(PSEL),
-        .PENABLE(PENABLE),
-        .PWDATA(PWDATA),
-        .PRDATA(PRDATA),
-        .PREADY(PREADY),
-        .IRQ(irq)
+            .ws(ws),
+            .sck(sck),
+            .sdi(sdi),
+            .PCLK(CLK),
+            .PRESETn(RESETn),
+            .PADDR(PADDR),
+            .PWRITE(PWRITE),
+            .PSEL(PSEL),
+            .PENABLE(PENABLE),
+            .PWDATA(PWDATA),
+            .PRDATA(PRDATA),
+            .PREADY(PREADY),
+            .IRQ(irq)
         );
     `endif // BUS_TYPE_APB
     `ifdef BUS_TYPE_AHB
@@ -42,20 +42,20 @@ module top();
         wire [31:0]	HRDATA;
         wire 		HREADY;
         EF_I2S_AHBL uut(
-        .ws(ws),
-        .sck(sck),
-        .sdi(sdi),
-        .HCLK(CLK), 
-        .HRESETn(RESETn), 
-        .HADDR(HADDR), 
-        .HWRITE(HWRITE), 
-        .HSEL(HSEL), 
-        .HTRANS(HTRANS), 
-        .HWDATA(HWDATA), 
-        .HRDATA(HRDATA), 
-        .HREADY(HREADY),
-        .HREADYOUT(HREADYOUT),
-        .IRQ(irq)
+            .ws(ws),
+            .sck(sck),
+            .sdi(sdi),
+            .HCLK(CLK), 
+            .HRESETn(RESETn), 
+            .HADDR(HADDR), 
+            .HWRITE(HWRITE), 
+            .HSEL(HSEL), 
+            .HTRANS(HTRANS), 
+            .HWDATA(HWDATA), 
+            .HRDATA(HRDATA), 
+            .HREADY(HREADY),
+            .HREADYOUT(HREADYOUT),
+            .IRQ(irq)
         );
     `endif // BUS_TYPE_AHB
     `ifdef BUS_TYPE_WISHBONE
@@ -66,9 +66,23 @@ module top();
         wire        cyc_i;
         wire        stb_i;
         reg         ack_o;
-        // TODO: initialize the Wishbone wrapper here
-        // for example
-        // EF_i2s_WB dut(.clk_i(CLK), .rst_i(~RESETn), .adr_i(adr_i), .dat_i(dat_i), .dat_o(dat_o), .sel_i(sel_i), .cyc_i(cyc_i), .stb_i(stb_i), .ack_o(ack_o), .we_i(we_i), .irq(irq));
+        
+        EF_I2S_WB dut(
+            .ws(ws),
+            .sck(sck),
+            .sdi(sdi),
+            .clk_i(CLK), 
+            .rst_i(~RESETn), 
+            .adr_i(adr_i), 
+            .dat_i(dat_i), 
+            .dat_o(dat_o), 
+            .sel_i(sel_i), 
+            .cyc_i(cyc_i), 
+            .stb_i(stb_i), 
+            .ack_o(ack_o), 
+            .we_i(we_i), 
+            .IRQ(irq)
+        );
     `endif // BUS_TYPE_WISHBONE
     // monitor inside signals
     `ifndef SKIP_WAVE_DUMP
